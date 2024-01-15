ead65fbea9a727aafcc75ff850c82d63243bc801
User <chaya.zilberstein@gmail.com>
2024-01-15 10:14:47 +0200

    first version vers-2

---
+++ Exe_2_Score_To_Char.vers_2/src/main/java/Exe_2_Score_To_Char/vers_2/Exe_2_Score_To_Char/vers_2/App.java
@@ -0,0 +1,17 @@
+package Exe_2_Score_To_Char.vers_2.Exe_2_Score_To_Char.vers_2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static String NumberToChar(int number) {
+		if ((50 <=number ) & (number <=59 ))
+			return "C";
+		else
+			return "error";
+				
+		
+	}
+}
---
+++ Exe_2_Score_To_Char.vers_2/src/test/java/Exe_2_Score_To_Char/vers_2/Exe_2_Score_To_Char/vers_2/AppTest.java
@@ -0,0 +1,28 @@
+package Exe_2_Score_To_Char.vers_2.Exe_2_Score_To_Char.vers_2;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;;
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+	@Test
+	public void exe_2_Partition_C_min_positive() {
+		
+		String result = App.NumberToChar(50);
+		String expected_result="C";
+		org.junit.Assert.assertEquals(expected_result,result,0.00001);
+	}
+	@Test
+	public void exe_2_Partition_C_min_plus1() {
+		
+		String result = App.NumberToChar(51);
+		String expected_result="C";
+		org.junit.Assert.assertEquals(expected_result,result,0.00001);
+	}
+	
+}
